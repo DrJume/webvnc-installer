c19209cf2bda61a532a0d65d0bf781815ce51d38
Julian Meinking <DrJume@users.noreply.github.com>
2020-01-14 03:34:16 +0100

    Update webvnc-installer.sh

--- webvnc-installer.sh
+++ webvnc-installer.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 # WebVNC Installer
+
+noVNC_version="1.1.0"
+x11vnc_version="0.9.16"
+
 if [ "$#" -gt 2 ]; then
   echo "Too many arguments!"
   echo
@@ -26,37 +30,45 @@ fi
 echo "[WebVNC Installer] Installing lubuntu-desktop (and other packages)"
 apt update && apt-get upgrade -y
 apt install -y lubuntu-desktop wget autoconf gcc pkg-config libssl-dev libx11-dev libxtst-dev libvncserver-dev screen automake python-minimal
+
 echo "[WebVNC Installer] Building and installing x11vnc"
 cd /root/
-wget https://github.com/LibVNC/x11vnc/archive/0.9.15.tar.gz
-tar xfvz 0.9.15.tar.gz
-cd x11vnc-0.9.15
+wget https://github.com/LibVNC/x11vnc/archive/$x11vnc_version.tar.gz
+tar xfvz $x11vnc_version.tar.gz
+cd x11vnc-$x11vnc_version
 ./autogen.sh
 make
 make install
+
 echo "[WebVNC Installer] Installing certbot"
 add-apt-repository ppa:certbot/certbot -y
 apt update
 apt install certbot -y
+
 echo "[WebVNC Installer] Registering HTTPS certificate"
 certbot certonly -n --standalone -d $1 --email $2 --agree-tos
+
 echo "[WebVNC Installer] Preparing certificate for noVNC"
 cd /etc/letsencrypt/live/$1/
 cat cert.pem >> certkey.pem
 cat privkey.pem >> certkey.pem
+
 echo "[WebVNC Installer] Installing noVNC"
 cd /root/
-wget https://github.com/novnc/noVNC/archive/v1.0.0.tar.gz
-tar xfvz v1.0.0.tar.gz
-echo "[WebVNC Installer] Writing start scripts"
-echo "screen -dmS novnc /root/noVNC-1.0.0/utils/launch.sh --vnc localhost:5900 --listen 443 --cert /etc/letsencrypt/live/"$1"/certkey.pem" > novnc.sh
-echo "screen -dmS x11vnc sudo x11vnc -norc -allow 127.0.0.1 -defer 10 -display :0 -forever -listen localhost -localhost -logfile /root/.x11vnc.log -wait 10 -xkb -auth /var/lib/lightdm/.Xauthority" > x11vnc.sh
+wget https://github.com/novnc/noVNC/archive/v$noVNC_version.tar.gz
+tar xfvz v$noVNC_version.tar.gz
+
+echo "[WebVNC Installer] Creating start scripts"
+echo "screen -dmS novnc /root/noVNC-$noVNC_version/utils/launch.sh --vnc localhost:5900 --listen 443 --cert /etc/letsencrypt/live/"$1"/certkey.pem" > novnc.sh
+echo "screen -dmS x11vnc sudo x11vnc -norc -allow 127.0.0.1 -defer 10 -find -forever -listen localhost -localhost -logfile /root/.x11vnc.log -wait 10 -xkb -auth /var/lib/lightdm/.Xauthority" > x11vnc.sh
 chmod +x novnc.sh x11vnc.sh
+
 echo "[WebVNC Installer] Setting lightdm hook to start x11vnc when login screen starts"
 cat > /etc/lightdm/lightdm.conf.d/50-x11vnc.conf <<EOL
 [Seat:*]
 greeter-setup-script=/root/x11vnc.sh
 EOL
+
 echo "[WebVNC Installer] Adding cronjob to start noVNC on system start"
 crontab -l | { cat; echo "@reboot /root/novnc.sh"; } | crontab -
 
